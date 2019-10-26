1678fa7b9334c831cd4980c8c22cd9be3f78e809
Mupati <scientificgh@gmail.com>
2019-10-26 22:17:49 +0000

    Single post design

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { Link } from "gatsby";
 
-const SinglePost = () => {
-  return <h3>Single Post</h3>;
+const SinglePost = ({ data }) => {
+  const { frontmatter } = data.markdownRemark;
+  return <h3>{frontmatter.date}</h3>;
 };
 
 export default SinglePost;
+
+export const blogQuery = graphql`
+  query SinglePost {
+    allMarkdownRemark(
+      filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            excerpt
+            date
+            title
+          }
+        }
+      }
+    }
+  }
+`;
