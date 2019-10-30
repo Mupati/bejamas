714e1d155944e349bbba6f62a7031c494d164719
Mupati <scientificgh@gmail.com>
2019-10-30 19:54:17 +0000

    Remove unnecessary files

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,4 +1,1 @@
-import CMS from "netlify-cms-app";
-import IndexPagePreview from "./preview-templates/IndexPagePreview";
-
-CMS.registerPreviewTemplate("index", IndexPagePreview);
+// import CMS from "netlify-cms-app";
